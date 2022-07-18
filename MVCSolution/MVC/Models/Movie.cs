bcd98092e531e8870142af197ab3125e7f8bdaa6
Joseph Monreal <josephmonreal@Josephs-MacBook-Pro.local>
2022-07-18 11:34:10 -0700

    Added EntityFrameworkCore & SQLLite

---
+++ MVCSolution/MVC/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
